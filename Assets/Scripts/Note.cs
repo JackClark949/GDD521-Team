bc0f3a86357219244c4f87a2cc91eb8376de5a49
DR676 <2306935@stu.chi.ac.uk>
2025-03-10 17:07:49 +0000

    Added HealthBar, PauseMenu and InteractableNote

---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public GameObject LettersUI;
+    bool toggle;
+
+    public void openCloseLetter()
+    {
+        toggle = !toggle;
+        if (toggle == false)
+        {
+            LettersUI.SetActive(false);
+        }
+        if(toggle == true)
+        {
+            LettersUI.SetActive(true);
+        }
+    }
+}
